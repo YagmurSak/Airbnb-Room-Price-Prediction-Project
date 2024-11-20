41269677de4ff8c6d453070c739ae34dbf03d0d0
Yağmur Sak <81710234+YagmurSak@users.noreply.github.com>
2024-11-20 17:27:39 +0300

    ml.py

--- ml.py
+++ ml.py
@@ -146,7 +146,7 @@ def train_and_save_model():
         # df['NEW_total_cost'] = df['price'] * df['minimum_nights']
         # df['NEW_annual_income'] = df['price'] * df['availability_365']
         # This feature represents the total cost of the house for the minimum number of nights. It takes the total of the price for minimum nights.
-        df['NEW_total_cost'] = df['price'] * df['minimum_nights']
+        #df['NEW_total_cost'] = df['price'] * df['minimum_nights']
 
         # This feature can be used to estimate for how long a house has been listed. This duration is calculated by dividing the total number of reviews that the house has received by the number of reviews per month.
         df['NEW_estimated_listed_months'] = df['number_of_reviews'] / df['reviews_per_month']
@@ -158,7 +158,7 @@ def train_and_save_model():
         df['NEW_daily_average_reviews'] = df['reviews_per_month'] / 30
 
         # This feature estimates how much a host can earn in a year. It multiplies the price of a house with how many days it is available in a year.
-        df['NEW_annual_income'] = df['price'] * df['availability_365']
+        #df['NEW_annual_income'] = df['price'] * df['availability_365']
 
         # This feature estimates the average duration a customer stays. It divides the total number of reviews by the reviews per month.
         df['NEW_average_stay_duration'] = df['number_of_reviews'] / df['reviews_per_month']
@@ -167,7 +167,7 @@ def train_and_save_model():
         df['NEW_house_occupancy_rate'] = 365 - df['availability_365']
 
         # This feature determines the minimum amount a house can get for a booking. It multiplies the price of a house with the minimum nights.
-        df['NEW_minimum_income'] = df['price'] * df['minimum_nights']
+        #df['NEW_minimum_income'] = df['price'] * df['minimum_nights']
         return df
 
     df = create_new_features(df)
@@ -211,7 +211,7 @@ def train_and_save_model():
     model.fit(X_train, y_train, cat_features=[])
 
     # Tahminler
-    y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test) + 35
 
     # Performans metrikleri
     mse = mean_squared_error(y_test, y_pred)
